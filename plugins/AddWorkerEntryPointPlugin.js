9fd7f1c5d2091f3a811323ef8b728893b272cd92
xiaoxudong <xiaoxudong@meituan.com>
2019-06-11 17:03:46 +0800

    first commit

---
+++ plugins/AddWorkerEntryPointPlugin.js
@@ -0,0 +1,48 @@
+const webpackVersion = require('webpack/package.json').version;
+const SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin');
+const LoaderTargetPlugin = require('webpack/lib/LoaderTargetPlugin');
+const WebWorkerTemplatePlugin = require('webpack/lib/webworker/WebWorkerTemplatePlugin');
+
+function getCompilerHook(compiler, {id, entry, filename, publicPath, chunkFilename, plugins}) {
+  return function(compilation, callback) {
+    const outputOptions = {
+      filename,
+      chunkFilename,
+      publicPath: publicPath || compilation.outputOptions.publicPath,
+      // HACK: globalObject is necessary to fix https://github.com/webpack/webpack/issues/6642
+      globalObject: 'this',
+    };
+    const childCompiler = compilation.createChildCompiler(id, outputOptions, [
+      new WebWorkerTemplatePlugin(),
+      new LoaderTargetPlugin('webworker'),
+    ]);
+    new SingleEntryPlugin(compiler.context, entry, 'main').apply(childCompiler);
+    plugins.forEach((plugin) => plugin.apply(childCompiler));
+
+    childCompiler.runAsChild(callback);
+  }
+}
+
+class AddWorkerEntryPointPlugin {
+  constructor({
+    id,
+    entry,
+    filename,
+    publicPath,
+    chunkFilename = undefined,
+    plugins = undefined,
+  }) {
+    this.options = { id, entry, filename, publicPath, chunkFilename, plugins };
+  }
+
+  apply(compiler) {
+    const compilerHook = getCompilerHook(compiler, this.options);
+    if (webpackVersion < '4') {
+      compiler.plugin('make', compilerHook);
+    } else {
+      compiler.hooks.make.tapAsync('AddWorkerEntryPointPlugin', compilerHook);
+    }
+  }
+}
+
+module.exports = AddWorkerEntryPointPlugin;
